efe57c8ac17cead34ff5addb86148fd4e758b9fe
andrevdv <220902110+andrevdv@users.noreply.github.com>
2026-01-13 12:31:33 +0100

    paths file

--- book/thesis_projects/MSc/2025_Q1_AndreVanDerVeen_CEG/work_in_progress/src/paths.py
+++ book/thesis_projects/MSc/2025_Q1_AndreVanDerVeen_CEG/work_in_progress/src/paths.py
@@ -1,1 +1,45 @@
-#empty for now
+from pathlib import Path
+
+# ------------------------------
+# Project Root
+# ------------------------------
+ROOT = Path(__file__).resolve().parents[1]  # work_in_progress
+
+# ------------------------------
+# Data folders
+# ------------------------------
+DATA = ROOT / "data"
+GRDC = DATA / "grdc"           
+BATHYMETRY = DATA / "bathymetry"
+SHAPEFILES = DATA / "shapefiles"
+
+
+# ------------------------------
+# Forcing
+# ------------------------------
+FORCING_FOLDER = ROOT / "forcing"
+FORCING_OUTPUT = FORCING_FOLDER / "output"
+
+
+# ------------------------------
+# Outputs
+# ------------------------------
+OUTPUTS = ROOT / "outputs"
+FIGURES = OUTPUTS / "figures"
+MODEL_OUTPUT = OUTPUTS / "model_runs"
+OUTPUT_HBV = MODEL_OUTPUT / "hbv"
+OUTPUT_PCRGLOB = MODEL_OUTPUT / "pcr-globwb"
+
+
+# ------------------------------
+# Notebooks
+# ------------------------------
+NOTEBOOKS = ROOT / "notebooks"
+
+# ------------------------------
+# Model Runs (intermediate folders)
+# ------------------------------
+MODEL_RUNS = ROOT / "model_runs"
+RUNS_HBV = MODEL_RUNS / "hbv"
+RUNS_LB = MODEL_RUNS / "leakybucket"
+RUNS_PCR = MODEL_RUNS / "pcrglobwb"
